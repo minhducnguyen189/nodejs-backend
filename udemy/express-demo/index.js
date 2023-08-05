9d68374df174adbffe46cb4a617440909b50aca1
Duc Nguyen <minhducnguyen189@gmail.com>
2023-08-05 17:07:56 +0700

    update nodejs example with express and joi

--- udemy/express-demo/index.js
+++ udemy/express-demo/index.js
@@ -1,2 +1,51 @@
-const express = require('express');
+const Joi = require("joi");
+const express = require("express");
 const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course 1" },
+  { id: 2, name: "course 2" },
+  { id: 3, name: "course 3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((ci) => ci.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given Id was not found");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details);
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+// app.get('/api/courses/:year/:month', (req, res) => {
+//     // res.send(req.params);
+//     res.send(req.query);
+// });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
